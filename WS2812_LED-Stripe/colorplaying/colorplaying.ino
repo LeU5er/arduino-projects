764b59b6eadf99de5383f0c3e87722ae98f4bafd
LeU5er <le@u5er.tk>
2016-12-30 09:31:25 +0100

    Add files via upload

---
+++ WS2812_LED-Stripe/colorplaying/colorplaying.ino
@@ -0,0 +1,131 @@
+//////////////////////////////////////////////////////////
+//// This little program is for Arduino together with ////
+//// the LED-Strip WS2812.                            ////
+//// Copyright: @LeU5er                               ////
+//// EMAIL: publicleu5er@gmail.com                    ////
+//// R4P3.NET: @LeU5er (SUPPORT VIA PM)               ////
+//// GITHUB: LeU5er                                   ////
+//////////////////////////////////////////////////////////
+
+#include "FastLED.h"
+#define SUM_LEDS 60
+#define LED_PIN 6
+#define DELAY_STD 10
+#define BRIGHTNESS_STD 127
+
+CRGBArray<SUM_LEDS> leds;
+
+void setup() {
+  FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, SUM_LEDS);
+}
+
+void loop() {  
+  // ########## FIRST ANIMATION ##########
+  // OVERWRITE BRIGHTNESS OF COLORS RED, BLUE, GREEN WITH 0
+  for(int i = 0; i < SUM_LEDS; i++) {
+    leds[i].r = 0;
+    leds[i].b = 0;
+    leds[i].g = 0;
+    FastLED.show();
+  }
+  // COLOR RED FROM 0 TO 30
+  for(int i = 0; i < SUM_LEDS/2; i++) {
+    leds[i].r = BRIGHTNESS_STD;
+    leds[SUM_LEDS-i].r = BRIGHTNESS_STD;
+    FastLED.show();
+    delay(DELAY_STD);
+  }
+  // OVERWRITE BRIGHTNESS OF COLOR RED WITH 0
+  for(int i = 0; i < SUM_LEDS/2; i++) {
+    leds[i].r = 0;
+    leds[SUM_LEDS-i].r = 0;
+    FastLED.show();
+    delay(DELAY_STD);
+  }
+  // COLOR BLUE FROM 0 TO 30
+  for(int i = 0; i < SUM_LEDS/2; i++) {
+    leds[i].b = BRIGHTNESS_STD;
+    leds[SUM_LEDS-i].b = BRIGHTNESS_STD;
+    FastLED.show();
+    delay(DELAY_STD);
+  }
+  // OVERWRITE BRIGHTNESS OF COLOR BLUE WITH 0
+  for(int i = 0; i < SUM_LEDS/2; i++) {
+    leds[i].b = 0;
+    leds[SUM_LEDS-i].b = 0;
+    FastLED.show();
+    delay(DELAY_STD);
+  }
+  // COLOR GREEN FROM 0 TO 30
+  for(int i = 0; i < SUM_LEDS/2; i++) {
+    leds[i].g = BRIGHTNESS_STD;
+    leds[SUM_LEDS-i].g = BRIGHTNESS_STD;
+    FastLED.show();
+    delay(DELAY_STD);
+  }
+  // OVERWRITE BRIGHTNESS OF COLOR GREEN WITH 0
+  for(int i = 0; i < SUM_LEDS/2; i++) {
+    leds[i].g = 0;
+    leds[SUM_LEDS-i].g = 0;
+    FastLED.show();
+    delay(DELAY_STD);
+  }
+  
+  // ########## SECOND ANIMATION ##########
+  // OVERWRITE BRIGHTNESS OF COLORS RED, BLUE, GREEN WITH 0
+  for(int i = 0; i < SUM_LEDS; i++) {
+    leds[i].r = 0;
+    leds[i].b = 0;
+    leds[i].g = 0;
+    FastLED.show();
+  }
+  // COLOR RED FROM 0 TO 30 & COLOR BLUE FROM 60 TO 30
+  for (int i = 0; i < SUM_LEDS/2; i++) {
+    leds[i].r = BRIGHTNESS_STD;
+    leds[SUM_LEDS-i].b = BRIGHTNESS_STD;
+    FastLED.show();
+    delay(DELAY_STD);
+  }
+  // OVERWRITE BRIGHTNESS OF COLORS RED, BLUE WITH 0
+  for(int i = 0; i < SUM_LEDS/2; i++) {
+    leds[i].r = 0;
+    leds[SUM_LEDS-i].b = 0;
+    FastLED.show();
+    delay(DELAY_STD);
+  }
+  // COLOR RED FROM 30 TO 60 & COLOR BLUE FROM 30 TO 0
+  for (int i = 0; i < SUM_LEDS/2; i++) {
+    leds[31+i].r = BRIGHTNESS_STD;
+    leds[30-i].b = BRIGHTNESS_STD;
+    FastLED.show();
+    delay(DELAY_STD);
+  }
+  // ----------- SECOND PART OF SECOND ANIMATION -----------
+  // OVERWRITE BRIGHTNESS OF COLORS RED, BLUE WITH 0
+  for(int i = 0; i < SUM_LEDS; i++) {
+    leds[i].r = 0;
+    leds[i].b = 0;
+    FastLED.show();
+  }
+  // COLOR RED FROM 60 TO 30 & COLOR BLUE FROM 0 TO 30
+  for (int i = 0; i < SUM_LEDS/2; i++) {
+    leds[SUM_LEDS-i].r = BRIGHTNESS_STD;
+    leds[i].b = BRIGHTNESS_STD;
+    FastLED.show();
+    delay(DELAY_STD);
+  }
+  // OVERWRITE BRIGHTNESS OF COLORS RED, BLUE WITH 0
+  for(int i = 0; i < SUM_LEDS/2; i++) {
+    leds[SUM_LEDS-i].r = 0;
+    leds[i].b = 0;
+    FastLED.show();
+    delay(DELAY_STD);
+  }
+  // COLOR RED FROM 30 TO 0 & COLOR BLUE FROM 30 TO 60
+  for (int i = 0; i < SUM_LEDS/2; i++) {
+    leds[30-i].r = BRIGHTNESS_STD;
+    leds[31+i].b = BRIGHTNESS_STD;    
+    FastLED.show();
+    delay(DELAY_STD);
+  }
+}
---
+++ WS2812_LED-Stripe/joystick/joystick.ino
@@ -0,0 +1,56 @@
+//////////////////////////////////////////////////////////
+//// This little program is for Arduino together with ////
+//// the LED-Strip WS2812.                            ////
+//// Copyright: @LeU5er                               ////
+//// EMAIL: publicleu5er@gmail.com                    ////
+//// R4P3.NET: @LeU5er (SUPPORT VIA PM)               ////
+//// GITHUB: LeU5er                                   ////
+//////////////////////////////////////////////////////////
+
+#include "FastLED.h"
+#define NUM_LEDS 60
+#define LED_PIN 6
+
+CRGBArray<NUM_LEDS> leds;
+
+void setup() {
+  FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, NUM_LEDS);
+}
+
+void loop() {
+
+  for(int i = 0; i < NUM_LEDS; i++) {
+    leds[i].r = 0;
+    leds[i].g = 0;
+    leds[i].b = 0;
+  }
+    int sensorValueA0 = analogRead(A0);  // vor-zurueck
+    int sensorValueA1 = analogRead(A1);  // links-rechts
+    int tempvar;
+    int temppos;
+
+    temppos = sensorValueA0/18;
+
+    //gruen
+    tempvar = 0xFF - sensorValueA1;
+    if (tempvar < 0) tempvar = 0;
+    leds[temppos].g = tempvar;
+    
+    //blau
+    tempvar = 0xFF-(1023-sensorValueA1);
+    if (tempvar < 0) tempvar = 0;
+    leds[temppos].b = tempvar;
+    
+    //rot
+    if (sensorValueA1 < 512) 
+    {
+    tempvar = 0xFF-sensorValueA1+512;
+    }
+    else 
+    {
+    tempvar = 0xFF-sensorValueA1-512;
+    }
+    leds[temppos].r = tempvar;
+    
+    FastLED.show();
+}
